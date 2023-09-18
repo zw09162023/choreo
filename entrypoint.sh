bb7ac2053b331c1ef31e59a3920459146328fab6
zw09162023 <145358656+zw09162023@users.noreply.github.com>
2023-09-18 21:50:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'52f3cad3-1628-4e7e-add4-afa266aaa07f'}
 WSPATH=${WSPATH:-'choreo'}
 
 # 生成配置文件，并经过 base64 加密
